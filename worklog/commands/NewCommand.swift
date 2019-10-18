2c6d3929c1f317395372cc9686ff808ab48d8115
Paul Traylor <kungfudiscomonkey@gmail.com>
2019-10-18 12:23:24 +0900

    Add colors to CLI

--- worklog/commands/NewCommand.swift
+++ worklog/commands/NewCommand.swift
@@ -8,21 +8,22 @@
 
 import SwiftCLI
 import Yams
+import Rainbow
 
 class NewCommand: Command {
     let name = "new"
     let path = Parameter()
     func execute() throws {
-        stdout <<< "Creating new hugo site"
+        stdout <<< "Creating new hugo site".yellow
         try shell("hugo", "new", "site", "--format", "yaml", path.value)
         changeCurrentDirectoryPath(path.value)
 
-        stdout <<< "Checking out themes"
+        stdout <<< "Checking out themes".yellow
         try shell("git", "init")
         try shell("git", "submodule", "add", "https://github.com/Xzya/hugo-bootstrap.git", "themes/bootstrap")
         try shell("git", "submodule", "add", "https://github.com/kfdm/hugo-worklog", "themes/worklog")
 
-        stdout <<< "Configuring Theme"
+        stdout <<< "Configuring Theme".yellow
         var config = try RawConfig.load(path: "config.yaml")
         config!["theme"] = ["bootstrap", "worklog"]
         try config?.dump(path: "config.yaml")
